31203a036419ac5b942ddb52e91ecba263b55f09
cadefihuilaltda <cadefihuilaltda@PT-0294.cadefihuila.local>
2019-05-12 21:50:50 -0500

    01 - Se crea la conexión con la base de datos oferta -  proyecto "Oferta"

---
+++ src/Conexion/TestConexion.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Conexion;
+
+/**
+ *
+ * @author cadefihuilaltda
+ */
+public class TestConexion {
+    public static void main(String[] args) {
+        conexion con=new conexion();
+        con.conectar();
+    }
+}
--- src/Conexion/conexion.java
+++ src/Conexion/conexion.java
@@ -5,10 +5,23 @@
  */
 package Conexion;
 
+import java.sql.*;
+
 /**
  *
- * @author cadefihuilaltda
+ * @author cadefihuilaltda (Delman Suarez)
  */
 public class conexion {
+    public Connection conex;
     
+    public Connection conectar(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conex = DriverManager.getConnection("jdbc:mysql://localhost/oferta","root","");
+            System.out.println("Conectado");
+        } catch (Exception e) {
+            System.out.println("No se pudo conectar la BD" + e.getMessage());
+        }
+        return conex;
+    }
 }
